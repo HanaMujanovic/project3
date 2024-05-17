446c342941166e275eb1b9ee34b2b7bcfb996fea
HanaMujanovic <166912620+HanaMujanovic@users.noreply.github.com>
2024-05-17 11:44:00 +0200

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,41 @@
+
+const myImg1 = document.getElementById("slika1");
+
+myImg1.addEventListener("click", function() {
+  myImg1.src = "media/pikachu.png";
+});
+
+
+const myImg2 = document.getElementById("slika2");
+
+myImg2.addEventListener("click", function() {
+  myImg2.src = "media/growlithe.png";
+});
+
+
+const myImg3 = document.getElementById("slika3");
+
+myImg3.addEventListener("click", function() {
+  myImg3.src = "media/jolteon.png";
+});
+
+
+const myImg4 = document.getElementById("slika4");
+
+myImg4.addEventListener("click", function() {
+  myImg4.src = "media/eevee.png";
+});
+
+
+const myImg5 = document.getElementById("slika5");
+
+myImg5.addEventListener("click", function() {
+  myImg5.src = "media/ponyta.jpg";
+});
+
+
+const myImg6 = document.getElementById("slika6");
+
+myImg6.addEventListener("click", function() {
+  myImg6.src = "media/diglett.jpg";
+});
